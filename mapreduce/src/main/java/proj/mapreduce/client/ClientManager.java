a5aa42912b0fb620a8dcb7e9d7ad587806f1581c
abhishekravi <abishek.ravichandran@gmail.com>
2016-04-05 15:06:22 -0400

    cleanup

--- mapreduce/src/main/java/proj/mapreduce/client/ClientManager.java
+++ mapreduce/src/main/java/proj/mapreduce/client/ClientManager.java
@@ -15,65 +15,64 @@ public class ClientManager {
 	static PingTask m_pingtask = null;
 	static FTPServer m_ftpserver = null;
 	static FtpClient m_ftpclient = null;
-	
+
 	static final int ftp_server_port = 8080;
-	
-	/* Run a thread to listen for received command in client/server tcp mode*/
-	public static void startObserver () throws UnknownHostException, IOException
-	{
-		if (m_pingtask == null) return;
-		
+
+	/* Run a thread to listen for received command in client/server tcp mode */
+	public static void startObserver() throws UnknownHostException, IOException {
+		if (m_pingtask == null)
+			return;
+
 		m_observer = new Observer(m_pingtask.serverAddress());
 		m_observer.start();
-		
+
 	}
-	
+
 	public static void startPinging() throws SocketException {
-		
+
 		m_pingtask = new PingTask();
 		m_pingtask.start();
 		m_busy = true;
 	}
 
 	public static void startFtpServer() {
-		
+
 		m_ftpserver = new FTPServer();
 		m_ftpserver.createFtpServer(ftp_server_port);
 		m_ftpserver.start();
-		
+
 	}
 
-	public static void getfromClient(String server, int port, String user, String password, String serverfile, String localfile) throws SocketException, IOException {
-		
+	public static void getfromClient(String server, int port, String user,
+			String password, String serverfile, String localfile)
+			throws SocketException, IOException {
+
 		m_ftpclient = new FtpClient(server, port, user, password);
-		m_ftpclient.downloadFileBlocking(serverfile, localfile);	
+		m_ftpclient.downloadFileBlocking(serverfile, localfile);
 		m_ftpclient.stop();
 	}
 
-	public static void getfromHdfs(String type, String accesskey, String secretkey, String bucketname, String key) throws IOException {
-		
-		switch (type)
-		{
+	public static void getfromHdfs(String type, String accesskey,
+			String secretkey, String bucketname, String key) throws IOException {
+
+		switch (type) {
 		case "aws":
-			
+
 			S3Reader reader = new S3Reader(accesskey, secretkey);
 			reader.readFromS3(bucketname, key);
-			
+
 			break;
 		default:
-				break;
+			break;
 		}
-		
+
 	}
 
 	public static void runJob() {
-		
-		
-		
+
 	}
-	
-	public static boolean busy ()
-	{
+
+	public static boolean busy() {
 		return m_busy;
 	}
 }
--- mapreduce/src/main/java/proj/mapreduce/utils/network/ftp/FTPServer.java
+++ mapreduce/src/main/java/proj/mapreduce/utils/network/ftp/FTPServer.java
@@ -20,49 +20,44 @@ import org.apache.ftpserver.usermanager.impl.WritePermission;
 public class FTPServer {
 
 	UserManager m_usermanager = null;
-	FtpServer 	m_ftpserver = null;
-	
-	
-	public void createFtpServer(int ftp_port)
-	{
+	FtpServer m_ftpserver = null;
+
+	public void createFtpServer(int ftp_port) {
 		Map<String, Ftplet> ftplet;
 		FtpServerFactory serverFactory;
 		ListenerFactory factory;
-		
+
 		factory = new ListenerFactory();
 		factory.setPort(ftp_port);
 
 		serverFactory = new FtpServerFactory();
 		serverFactory.addListener("default", factory.createListener());
-		
-		if (m_usermanager == null)
-		{
+
+		if (m_usermanager == null) {
 			createUserManager();
 			addUser("test", "test", "/home/test");
 		}
 		serverFactory.setUserManager(m_usermanager);
-		
+
 		ftplet = new HashMap<String, Ftplet>();
 		ftplet.put("miaFtplet", new CostumFtplet());
 		serverFactory.setFtplets(ftplet);
-		
+
 		m_ftpserver = serverFactory.createServer();
 	}
 
-
-	public void createUserManager()
-	{
+	public void createUserManager() {
 		PropertiesUserManagerFactory userManagerFactory;
 
 		userManagerFactory = new PropertiesUserManagerFactory();
-		userManagerFactory.setFile(new File(System.getProperty("user.dir") + "/myusers.properties"));
+		userManagerFactory.setFile(new File(System.getProperty("user.dir")
+				+ "/myusers.properties"));
 		userManagerFactory.setPasswordEncryptor(new CostumPasswordEncryptor());
 
 		m_usermanager = userManagerFactory.createUserManager();
 	}
 
-	public void createUserManager(String path)
-	{
+	public void createUserManager(String path) {
 		PropertiesUserManagerFactory userManagerFactory;
 
 		userManagerFactory = new PropertiesUserManagerFactory();
@@ -72,37 +67,29 @@ public class FTPServer {
 		m_usermanager = userManagerFactory.createUserManager();
 	}
 
-	public void addUser(String username, String password, String homedir)
-	{
+	public void addUser(String username, String password, String homedir) {
 		List<Authority> authorities = new ArrayList<Authority>();
 		BaseUser user = new BaseUser();
-		
+
 		authorities.add(new WritePermission());
-		
+
 		user.setName(username);
 		user.setPassword(password);
 		user.setHomeDirectory(homedir);
 		user.setAuthorities(authorities);
 
-		try
-		{
+		try {
 			m_usermanager.save(user);
-		}
-		catch (FtpException e1)
-		{
+		} catch (FtpException e1) {
 			;
 		}
 
 	}
 
-	public void start ()
-	{	
-		try
-		{
+	public void start() {
+		try {
 			m_ftpserver.start();
-		}
-		catch (FtpException ex)
-		{
+		} catch (FtpException ex) {
 			;
 		}
 	}
