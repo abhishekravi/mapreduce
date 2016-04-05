e0472191ab195a09058b80ce548fb7e1873d85be
abhishekravi <abishek.ravichandran@gmail.com>
2016-04-05 15:38:28 -0400

    adding main class

---
+++ mapreduce/src/main/java/proj/mapreduce/main/Client.java
@@ -0,0 +1,17 @@
+package proj.mapreduce.main;
+
+
+
+import java.io.IOException;
+
+import proj.mapreduce.client.ClientManager;
+
+public class Client {
+	
+	public static void main(String[] args) throws IOException {
+		ClientManager.startPinging();
+		
+		while (ClientManager.busy());
+		
+	}
+}
