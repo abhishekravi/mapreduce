ef1b5593517146059ba9408017f24b6b9ef2f8c8
maniaabdi <mania.abdi287@gmail.com>
2016-04-06 23:02:26 -0400

    Update Server

--- mapreduce/src/main/java/proj/mapreduce/utils/network/NetworkDiscovery.java
+++ mapreduce/src/main/java/proj/mapreduce/utils/network/NetworkDiscovery.java
@@ -1,7 +1,14 @@
 package proj.mapreduce.utils.network;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
+
+import proj.mapreduce.server.Listener;
 
 /**
  * Class to discover networks in the same subnet.
@@ -11,18 +18,108 @@ import java.util.HashMap;
  */
 public class NetworkDiscovery {
 	
-	private HashMap<InetAddress, Boolean> 	m_neighbors; 
+	private static HashMap<InetAddress, Boolean> 	m_neighbors; 
+	private final static int m_port = 54321;
+	private static boolean m_active = false;
 	
-	public static HashMap<InetAddress, Boolean> discover()
+	public HashMap<InetAddress, Boolean> discover()
+	{
+		
+		DatagramSocket bcsocket = createDatagramConnection ();
+		sendDiscoveryPacket (bcsocket);
+		
+		listen (bcsocket);
+		
+		return null;
+	}
+	
+	public HashMap<InetAddress, Boolean> discover(String inet)
 	{
 		
 		return null;
 	}
 	
-	public static HashMap<InetAddress, Boolean> discover(String inet)
+	private DatagramSocket createDatagramConnection () 
 	{
-		
-		return null;
+		 DatagramSocket bcsocket = null;
+		try {
+			bcsocket = new DatagramSocket(m_port);
+			bcsocket.setBroadcast(true);
+			bcsocket.connect(InetAddress.getByName("255.255.255.255"), m_port);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		 
+		 return bcsocket;
 	}
 	
+	private void sendDiscoveryPacket(DatagramSocket socket)
+	{
+		byte [] buf = Command.YARN_DETECT.toString().getBytes();
+		DatagramPacket discovermsg = new DatagramPacket(buf, buf.length);
+		
+		try {
+			socket.send(discovermsg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void listen (DatagramSocket socket)
+	{
+		final DatagramSocket bcsocket = socket;
+		
+		Runnable listener = new Runnable () {			
+			@Override
+            public void run() {
+				byte[] buf = new byte [8192];
+				DatagramPacket replypacket = new DatagramPacket(buf, buf.length);
+				String replymsg;
+				
+				m_active = true;
+				
+				while (m_active)
+				{
+					try {
+						bcsocket.receive(replypacket);
+						replymsg = replypacket.getData().toString() + ":" + 
+								replypacket.getAddress().toString() + ":" + 
+								replypacket.getPort();
+						
+						Listener.takeAction(replymsg);
+						
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+            }
+		};
+		
+		Thread listenerth = new Thread(null, listener, "balalalala");
+		listenerth.start();
+		
+		try {
+			listenerth.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void updateneighbors (String address)
+	{
+		try {
+			m_neighbors.put(InetAddress.getByName(address), true);
+			
+			
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
